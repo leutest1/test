eb109d89a608e8bc448b0f521fd9855cf0c87e34
lducanh1995 <lducanh1995@gmail.com>
2018-02-25 15:17:34 +0700

    1995

--- src/Test4.java
+++ src/Test4.java
@@ -4,8 +4,9 @@
 public class Test4 {
     public static void main(String[] args) {
         System.out.println("a");
+        System.out.println("b");
         System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
+        System.out.println("b");
+
     }
 }
