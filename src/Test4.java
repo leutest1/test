7ae5c9b0d126e302b7f441a3457cd92bac688f52
MAYTINH-FALE4NV\MyPC <leuaction@gmail.com>
2018-02-25 15:05:42 +0700

    actoin

--- src/Test4.java
+++ src/Test4.java
@@ -2,4 +2,10 @@
  * Created by MyPC on 25/02/2018.
  */
 public class Test4 {
+    public static void main(String[] args) {
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+    }
 }
